650209ef773ffbb0c0d77152ad0f455b0835df4c
Pedro Henrique Lage Duarte <duartep@google.com>
2019-08-15 17:34:15 -0300

    paramsVariaveis

--- exercicios-web/funcao/paramsVariaveis.js
+++ exercicios-web/funcao/paramsVariaveis.js
@@ -6,9 +6,20 @@ function soma() {
     return soma
 }
 
+function imprimir() {
+    for (const key in arguments) {
+        console.log(arguments[key])
+    }
+}  
+
+
+
 console.log(soma())
 console.log(soma(1))
 console.log(soma(1.1, 2.2, 3.3))
 
 console.log(soma(1.1, 2.2, "Teste"))
 console.log(soma('a', 'b', 'c'))
+
+imprimir(1,3,4,5,6,7,8,9,10,100)
+imprimir("teste")
