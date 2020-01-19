b735cffa9eb51d0ba76aeebea6a066994dd42f48
phdduarte <phdduarte@hotmail.com>
2020-01-19 16:01:39 -0300

    End Capitulo Funcao - aula 1 - Cidadao Primeira Classe

--- exercicios-web/funcao/1 - cidadaoPrimeiraClasse.js
+++ exercicios-web/funcao/1 - cidadaoPrimeiraClasse.js
@@ -1,11 +1,23 @@
-// Função em JS é First-Class Object (Citizens)
-// Higher-order function
+/**
+ * Anotacoes da aula cidadao de primeira classe
+ * 
+ * Função em JS é First-Class Object (Citizens)
+ * Higher-order function
+ * Isso significa que voce pode tratar funcao como um objeto.
+ * Antigamente ficava separado o objeto e a funcao e agora eles podem ser tratados 
+ * Podemos trabalhar dentro do JS - POO, funcional, estrutural.
+ * 
+ * Author: Pedro Duarte
+ * 
+ */
+
 
 // criar de forma literal
+// Retorna por padrão undefined
 function fun1() { }
 
 // Armazenar em uma variável
-const fun2 = function () { }
+const fun2 = function () { } // Aqui é uma funcao anonima para voce chamar pode chamar a constante com ()
 
 // Armazenar em um array
 const array = [function (a, b) { return a + b }, fun1, fun2]
